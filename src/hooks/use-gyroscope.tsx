a464f9edfc9d6132d447ba13d7283e84f980866f
tiwari47 <vikashti7580@gmail.com>
2026-09-02 19:35:33 +0530

    vikash tiwari

--- src/hooks/use-gyroscope.tsx
+++ src/hooks/use-gyroscope.tsx
@@ -1,30 +1,29 @@
 import { Gyroscope } from "expo-sensors";
 import { useEffect, useState } from "react";
 
-export function useGyroscope(){
-    const [available,setAvailable]  = useState<boolean | null>(null)
-    const [x,setX] = useState(0);
-    const [y,setY] = useState(0);
-    const [z,setZ] = useState(0);
+export function useGyroscope() {
+  const [available, setAvailable] = useState<boolean | null>(null);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [z, setZ] = useState(0);
+  console.log("values updated");
+  useEffect(() => {
+    let subsciption: { remove: () => void } | undefined;
 
-    useEffect(()=>{
-        let subsciption : {remove:()=>void} | undefined;
+    (async () => {
+      const isAvailable = await Gyroscope.isAvailableAsync();
+      setAvailable(isAvailable);
+      if (!isAvailable) return;
 
-        (async()=>{
-            const isAvailable = await Gyroscope.isAvailableAsync()
-            setAvailable(isAvailable)
-            if(!isAvailable) return;
+      Gyroscope.setUpdateInterval(100);
+      subsciption = Gyroscope.addListener((data) => {
+        setX(data.x);
+        setY(data.y);
+        setZ(data.z);
+      });
+    })();
+    return () => subsciption?.remove();
+  }, []);
 
-            Gyroscope.setUpdateInterval(100);
-            subsciption = Gyroscope.addListener((data)=>{
-                 setX(data.x)
-                 setY(data.y)
-                 setZ(data.z)
-            })
-
-        })()
-        return ()=>subsciption?.remove()
-    },[])
-    
-  return { available,x,y,z}
+  return { available, x, y, z };
 }
